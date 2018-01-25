dfedd0b75c5dfb27356ac7e2739f920c079eccf1
JOSEPH REYNA <JOSREYNA@uat.edu>
2018-01-25 12:56:16 -0700

    Add project files.

---
+++ Project1/Source.cpp
@@ -0,0 +1,30 @@
+// Guess My number 
+// Number guessing game 
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+	srand(static_cast<unsigned int>(time(0)));
+
+	int secretNumber = rand() % 100 + 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
