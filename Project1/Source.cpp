a78a7be63152ba7f337de3c38bf4d355ff937627
JOSEPH REYNA <josreyna@uat.edu>
2018-01-25 13:22:57 -0700

    First Check in

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -11,20 +11,31 @@ int main()
 {
 	srand(static_cast<unsigned int>(time(0)));
 
-	int secretNumber = rand() % 100 + 1;
+	int secretNumber = rand() % 100 + 1; // Random number between 1 and 100
+	int tries = 0;
+	int guess;
 
+	cout << "\tWelcome To Guess My Number\n\n";
 
+	do
+	{
+		cout << "enter a guess: ";
+		cin >> guess;
+		++tries;
 
-
-
-
-
-
-
-
-
-
-
+		if (guess > secretNumber)
+		{
+			cout << "Too High!\n\n";
+		}
+		else if (guess < secretNumber)
+		{
+			cout << "Too low!\n\n";
+		}
+		else
+		{
+			cout << "\nThat's it! you got it " << tries << "guesses!\n";
+		}
+	} while (guess != secretNumber);
 
 	return 0;
 }
